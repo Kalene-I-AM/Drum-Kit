8e19136c775d22e0b2c7a61bb3985928aa53cad0
Kalene-I-AM <137322993+Kalene-I-AM@users.noreply.github.com>
2024-04-15 15:42:29 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const audioContainer = document.getElementById('audioContainer');
+
+const audioContainer2 = document.getElementById('audioContainer2');
+
+const audioContainer3 = document.getElementById('audioContainer3');
+
+const audioContainer4 = document.getElementById('audioContainer4');
+
+const audioContainer5 = document.getElementById('audioContainer5');
+
+const audioContainer6 = document.getElementById('audioContainer6');
+
+const audioContainer7 = document.getElementById('audioContainer7');
+
+const audioContainer8 = document.getElementById('audioContainer8');
+
+
+function playBass() {
+    audioContainer.play();
+}
+
+
+
+function playSnare() {
+    audioContainer2.play();
+}
+
+function playFloor() {
+    audioContainer3.play()
+}
+
+function playTom1() {
+    audioContainer4.play()
+}
+
+function playTom2() {
+    audioContainer5.play();
+}
+
+
+function playHi() {
+    audioContainer6.play();
+}
+
+function playCrash() {
+    audioContainer7.play();
+}
+
+function playRide() {
+    audioContainer8.play();
+}
